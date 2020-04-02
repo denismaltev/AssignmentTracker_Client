fa0988b34e5575456eb201331e6fcef9477d7651
Den79 <test08081979@gmail.com>
2020-04-01 18:35:36 -0700

    mark  "Assignment Done" changes on click

--- homework-tracker/src/components/Assignment.js
+++ homework-tracker/src/components/Assignment.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
 
 export default function Assignment(props) {
+  const [refreshComponent, setRefreshComponent] = useState(false);
   const deleteAssignment = async () => {
     // No logic yet
     alert("DELETE request to DB");
   };
+  const editAssignment = async () => {
+    // No logic yet
+    alert("PUT request to DB");
+  };
+
+  function isDoneChange() {
+    if (props.assignment.isDone) {
+      props.assignment.isDone = false;
+    } else {
+      props.assignment.isDone = true;
+    }
+    setRefreshComponent(true);
+  }
+
+  useEffect(() => {
+    setRefreshComponent(false);
+  }, [refreshComponent]);
 
   return (
     <div>
-      <p onClick={deleteAssignment} style={{ color: "red" }}>
-        [X]
-      </p>
+      <Button onClick={editAssignment} style={{ color: "green" }}>
+        E
+      </Button>
+      <Button onClick={deleteAssignment} style={{ color: "red" }}>
+        X
+      </Button>
       {props.assignment.isDone ? (
         <strike>
           <h4>
-            <p>{props.assignment.title}</p>
+            <div onClick={isDoneChange}>{props.assignment.title}</div>
           </h4>
         </strike>
       ) : (
-        <h4>
-          <p>{props.assignment.title}</p>
-        </h4>
+        <div>
+          <h4>
+            <div onClick={isDoneChange}>{props.assignment.title}</div>
+          </h4>
+        </div>
       )}
       <p>{props.assignment.description}</p>
       <p>{props.assignment.data}</p>
