9458b4c846e0ca95d82214c97f8b908eae45801f
Den79 <test08081979@gmail.com>
2020-04-01 17:43:23 -0700

    Filter Active/Complited Done

--- homework-tracker/src/components/Assignment.js
+++ homework-tracker/src/components/Assignment.js
@@ -1,10 +1,27 @@
 import React from "react";
 
 export default function Assignment(props) {
-  console.log(props.assignment.title);
+  const deleteAssignment = async () => {
+    // No logic yet
+    alert("DELETE request to DB");
+  };
+
   return (
     <div>
-      <p>{props.assignment.title}</p>
+      <p onClick={deleteAssignment} style={{ color: "red" }}>
+        [X]
+      </p>
+      {props.assignment.isDone ? (
+        <strike>
+          <h4>
+            <p>{props.assignment.title}</p>
+          </h4>
+        </strike>
+      ) : (
+        <h4>
+          <p>{props.assignment.title}</p>
+        </h4>
+      )}
       <p>{props.assignment.description}</p>
       <p>{props.assignment.data}</p>
       <p>=============================</p>
