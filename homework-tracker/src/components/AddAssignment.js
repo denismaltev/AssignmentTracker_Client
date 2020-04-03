ae5ffd7082ecd89dbe0ca0df912dce43e7e90104
Den79 <test08081979@gmail.com>
2020-04-02 21:00:55 -0700

    Validation

--- homework-tracker/src/components/AddAssignment.js
+++ homework-tracker/src/components/AddAssignment.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Logo from "../assets/Logo.png";
 import { Form, Button } from "react-bootstrap";
 
 export default function AddAssignment() {
+  const [errorMessage, setErrorMesage] = useState("");
+
   const createAssignment = async event => {
     event.preventDefault();
     const { title, description, date } = event.target.elements;
-
+    // Validation
+    if (title.value === "" || date.value === "") {
+      setErrorMesage("Error: Please fill all required fields");
+    } else {
+      setErrorMesage("");
+      alert(`POST-request: ${title.value} ${description.value} ${date.value}`);
+    }
     // Here should be POST-request
-    alert(`POST-request: ${title.value} ${description.value} ${date.value}`);
     document.getElementById("add-assignment-form").reset();
   };
 
@@ -16,7 +23,7 @@ export default function AddAssignment() {
     event.preventDefault();
     document.getElementById("add-assignment-form").reset();
   };
-
+  useEffect(() => {}, [errorMessage]);
   return (
     <div>
       <div className="header">
@@ -27,6 +34,7 @@ export default function AddAssignment() {
       </div>
       <div className="Card">
         <form onSubmit={createAssignment} id="add-assignment-form">
+          <p style={{ color: "red", fontSize: 12 }}>{errorMessage}</p>
           <input name="title" type="text" placeholder="Title" />
           <input name="description" type="text" placeholder="Description" />
           <input name="date" type="date" placeholder="Due Date" />
