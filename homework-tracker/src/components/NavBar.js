2739b4e390c38a3e33faa8cb8b051242780eae56
taranakajima <nakatarat@gmail.com>
2020-04-06 11:54:23 -0700

    Styled NavBar

--- homework-tracker/src/components/NavBar.js
+++ homework-tracker/src/components/NavBar.js
@@ -1,19 +1,21 @@
 import React, { useContext } from 'react';
 import app from './authentication/firebase';
 import { AuthContext } from './authentication/Auth';
+import { withRouter } from 'react-router-dom';
 
-const NavBar = () => {
+const NavBar = ({ history }) => {
   const { currentUser } = useContext(AuthContext);
 
 
   return (
-    <div>
+    <div className="NavBar__container">
+      <button className="NavBar__item" onClick={() => history.push('/')}>Home</button>
       {(currentUser) ? (
-        <button onClick={() => app.auth().signOut()}>Logout</button>
+        <button className="NavBar__item" onClick={() => app.auth().signOut()}>Logout</button>
       ) : null
       }
     </div>
   );
 }
 
-export default NavBar;
+export default withRouter(NavBar);
