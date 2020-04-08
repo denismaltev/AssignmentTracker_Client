eacfc8af7271854ddcf6a117c5f60361820e4c76
Athena <athenakozak@gmail.com>
2020-04-08 16:06:13 -0700

    #31 Adding put req for isDone **BUGS**

--- homework-tracker/src/components/Assignment.js
+++ homework-tracker/src/components/Assignment.js
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPen, faTimes, faBell } from "@fortawesome/free-solid-svg-icons";
 import { faCircle, faCheckCircle } from "@fortawesome/free-regular-svg-icons";
 import { Link } from "react-router-dom";
+import firebase from "./authentication/firebase";
 
 export default function Assignment(props) {
   const [refreshComponent, setRefreshComponent] = useState(false);
@@ -18,18 +19,66 @@ export default function Assignment(props) {
     if (props.assignment.isDone) {
       props.assignment.isDone = false;
       setNotification("notification-incomplete")
+      //postIsDone()
     } else {
       props.assignment.isDone = true;
       setNotification("notification-complete")
+      //postIsDone()
     }
     setRefreshComponent(true);
   }
+
+  const API_URL = process.env.REACT_APP_API_URL;
+  
+  // NEEDS WORK
+  const postIsDone = async event => {
+    //event.preventDefault();
+    const {isDone} = event.target.elements;
+    let JWTtoken = await (
+      await firebase.auth().currentUser.getIdTokenResult()
+    ).token;
+    if(JWTtoken !== null) {
+      const id = props.assignment._id
+      console.log(id)
+      const result = await fetch(API_URL + "assignments/" + id, {
+        method: "PUT",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${JWTtoken}`
+        },
+        body: JSON.stringify({
+          isDone: isDone.value
+        })
+      });
+      if(result.status === 200) {
+        alert("Updated Assignment!");
+      } else {
+        alert("Error: Something went wrong, please try again")
+      }
+    }
+  document.getElementById("add-assignment-form").reset();
+};
+
   function isAssignmentExpired() {
     return today.getTime() - props.assignment.date.getTime() > 0 &&
       !props.assignment.isDone
       ? true
       : false;
   }
+
+  // function NotificationStatus() {
+  //   if(props.assignment.isDone) {
+  //     setNotification("notification-complete")
+  //   } else if(isAssignmentExpired && !props.assignment.isDone) {
+  //     setNotification("notification-late")
+  //   } else if(!props.assignment.isDone && daysLeft() === 0) {
+  //     setNotification("notification-today")
+  //   } else {
+  //     setNotification("notification-incomplete")
+  //   }
+  //   setRefreshComponent(true)
+  // }
 
   function daysLeft() {
     return Math.ceil((props.assignment.date - today) / 1000 / 60 / 60 / 24);
